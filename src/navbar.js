6f51b9a46ff18a44df5c173b1ecb9fb27e7dc011
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-28 22:52:38 +0200

    Add class list names and change a tag to button.

--- src/navbar.js
+++ src/navbar.js
@@ -1,12 +1,12 @@
 
 const navbar = () => {
-     const navlinks = ['home','menu','contact'];
 
-     const navList = document.createElement('ul')
 
+     const navList = document.createElement('ul');
+      navList.classList.add("d-flex", "justify-content-around","list-unstyled","p-3");
 
      const itemOne = document.createElement('li');
-     const alink = document.createElement("a")
+     const alink = document.createElement("button")
      alink.innerHTML = "HOME"
      alink.className = 'nav-link'
      alink.id = 'home'
@@ -14,7 +14,7 @@ const navbar = () => {
      itemOne.append(alink)
      
      const itemTwo = document.createElement("li");
-     const blink = document.createElement("a");
+     const blink = document.createElement("button");
      blink.innerHTML = "MENU";
      blink.className = "nav-link";
      blink.id = "menu";
@@ -22,7 +22,7 @@ const navbar = () => {
      itemTwo.append(blink);
 
      const itemThree = document.createElement("li");
-     const clink = document.createElement("a");
+     const clink = document.createElement("button");
      clink.innerHTML = "CONTACT";
      clink.className = "nav-link";
      clink.id = "contact";
