7d416aa043410cb1d2d3ee94bf32be83431c38cb
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-07-28 23:42:54 +0200

    Add google map address embedded code

--- src/contact.js
+++ src/contact.js
@@ -44,16 +44,7 @@ const contactPage = () => {
         </form>
       </div>
     </div>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d214758.79711848433!2d-117.2340910766061!3d32.74964580211821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dbfe28190f4b67%3A0x92fa4bca315a5951!2sMcDonald&#39;s!5e0!3m2!1sen!2sus!4v1601955396977!5m2!1sen!2sus"
-      width="600"
-      height="450"
-      frameborder="0"
-      style="border: 0"
-      allowfullscreen=""
-      aria-hidden="false"
-      tabindex="0"
-    ></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3575.5272280991567!2d27.855745014744457!3d-26.341804175978364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e95a9961e41864d%3A0xf07d0a6683223795!2sDelhi%20Delicious%20Headoffice!5e0!3m2!1sen!2sza!4v1627508384711!5m2!1sen!2sza" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
   </div>`;
 
   contactContainer.appendChild(contact);
